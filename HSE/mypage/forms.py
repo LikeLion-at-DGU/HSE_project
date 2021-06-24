f8ad08b64e0cb9e2addb9fb164de0bdb116221a2
tkdwns414 <sangjune2000@naver.com>
2021-06-25 00:44:43 +0900

    Fix: 이름-> name

--- HSE/mypage/forms.py
+++ HSE/mypage/forms.py
@@ -2,15 +2,18 @@ from django.contrib.auth.models import User
 from django import forms
 from .models import Profile
 
-class SignupForm(forms.Form):
-    model=User
 
-    name = forms.CharField(max_length=64, widget=forms.TextInput(attrs={'placeholder':'name'}))
+class SignupForm(forms.Form):
+    model = User
+
+    name = forms.CharField(
+        max_length=64, widget=forms.TextInput(attrs={"placeholder": "name"})
+    )
 
     def signup(self, request, user):
-        profile=Profile()
+        profile = Profile()
         profile.user = user
-        profile.name = self.cleaned_data['이름']
+        profile.name = self.cleaned_data["name"]
         profile.save()
         user.save()
         return user
