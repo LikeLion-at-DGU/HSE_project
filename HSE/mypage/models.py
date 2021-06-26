7e0c35070a96432639320cf8c37eb8d59a2c50f5
kkong1007 <vkslvkd1007@naver.com>
2021-06-26 17:01:29 +0900

    mypage 수정(기능은 주석처리)

--- HSE/mypage/models.py
+++ HSE/mypage/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
-#from apply.models import apply
+from apply.models import apply
 
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=64, default="")
 
-#class mypage(models.Model):
+    #number=models.IntegerField(default=0)
+    #title=models.CharField(max_length=200)
+    #main_or_sub = models.CharField(max_length=10)
+    #date = models.DateField()
+    #hour = models.IntegerField(default=0)
--- HSE/mypage/views.py
+++ HSE/mypage/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
 from main.models import Post
 from django.contrib.auth.models import User
+
+from .models import Profile
+from apply.models import apply
+from django.db.models import Sum, Count
 # Create your views here.
 
 def mypage(request):
     user = request.user
     posts=Post.objects.filter(writer=user)
+
+    #total_time = Profile.objects.values('hour').aggregate(Sum)
+    #total_count = Profile.objects.values('pk').annotate(Count('pk'))
+    
+    #if user == apply.applicant:
+        #main_or_sub = request.apply['main_or_sub']
+        #title = request.apply['title']
+        #date = request.apply['date']
+        #hour = request.apply['hour']
     return render(request, 'mypage/mypage.html', {'posts':posts})
