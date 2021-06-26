1119f07f2b19625ec602717d794528649fa8ff14
kkong1007 <vkslvkd1007@naver.com>
2021-06-26 21:40:28 +0900

    mypage 수정중

--- HSE/mypage/views.py
+++ HSE/mypage/views.py
@@ -10,17 +10,18 @@ from django.db.models import Sum, Count
 
 def mypage(request):
     user = request.user
-    posts=Post.objects.filter(writer=user)
+    #posts=Post.objects.filter(writer=user)
+    apply_title = request.apply['title', '']
     result1 = apply.objects.filter(winner=user)
-    #result2 = EduPost.objects.filter(title=)
+    result2 = EduPost.objects.filter(title=apply_title)
 
     #result2.total_time = EduPost.objects.filter('work_hour').aggregate(Sum)
     #total_count = Profile.objects.values('pk').annotate(Count('pk'))
     
-    main_or_sub = result1.main_or_sub
+    #main_or_sub = result1.main_or_sub
     #if user == apply.winner:
     #result1.main_or_sub = apply.objects.filter('main_or_sub')
     #result1.title = apply.objects.filter('title')
     #result2.date = EduPost.objects.filter('work_date')
     #result2.hour = EduPost.objects.filter('work_hour')
-    return render(request, 'mypage/mypage.html', {'posts':posts})
+    return render(request, 'mypage/mypage.html', {'result1':result1, 'result2':result2})
