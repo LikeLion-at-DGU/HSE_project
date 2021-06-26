6674bf033b95caf1da41cd91fc52c46bbe571926
LeeTaeGeom <taegeom0601@gmail.com>
2021-06-26 13:56:10 +0900

    random 수정

--- HSE/apply/views.py
+++ HSE/apply/views.py
@@ -4,6 +4,38 @@ from education.models import EduPost
 import random
 import datetime
 
+count={}
+
+
+def get_c(c):
+    count[c]=0
+class c:
+    def __init__(self):
+        self.main_l=[]
+        self.sub_l=[]
+
+    def get_m(self,z):
+        self.main_l.append(z)
+        
+    def get_s(self,z):
+        self.sub_l.append(z)
+       
+    def fin_m(self,z):
+        self.main_l=z
+        
+        
+    def fin_s(self,z):
+        self.sub_l=z
+        
+    def put_m(self):
+        
+        return self.main_l
+    def put_s(self):
+        
+        return self.sub_l
+
+m=c()
+
 
 
 def apply_main_sub(request, post_id):
@@ -21,34 +53,47 @@ def apply_new(request):
     new_apply.save()
     return redirect('education:edulist')
 
+
 def apply_result(request,post_id):
     
-    post=get_object_or_404(EduPost,pk=post_id)
-    apply_o=apply.objects.all()
     
-    t=[] #특정(현재 post_id 봉사관련) db 만 골라내기
-    main_l=[]
-    sub_l=[]
-    
-    for i in apply_o:
-        if i.title  == post.title:
-            t.append(i)
-    
-    for i in t:
-        if i.main_or_sub == '주강사':
-            main_l.append(i)
-            
-        else:
-            sub_l.append(i)
+    if count[post_id]==0:
+        post=get_object_or_404(EduPost,pk=post_id)
+        apply_o=apply.objects.all()
         
-  
-    if post.main_teacher < len(main_l): 
+        t=[] #특정(현재 post_id 봉사관련) db 만 골라내기
+       
         
-        main_l=random.sample(main_l,post.main_teacher) #아니면 다뽑음
+        for i in apply_o:
+            if i.title  == post.title:
+                t.append(i)
         
+        for i in t:
+            if i.main_or_sub == '주강사':
+                m.get_m(i)
+            else:
+                m.get_s(i)
+        # for i in m.put_m():
+        #     print("안에서 검사",i.name)   
+        # for i in m.put_s():
+        #     print("안에서 검사z",i.name) 
 
-    if post.sub_teacher < len(sub_l): 
-        sub_l=random.sample(sub_l,post.sub_teacher)  #아니면다뽑음
-    
-    
+        if post.main_teacher < len(m.put_m()): 
+            main_l=random.sample(m.put_m(),post.main_teacher) #아니면 다뽑음
+           
+            m.fin_m(main_l)
+        if post.sub_teacher < len(m.put_s()): 
+            sub_l=random.sample(m.put_s(),post.sub_teacher)  #아니면다뽑음
+            
+            m.fin_s(sub_l)
+        count[post_id]+=1
+        
+    else:
+       
+        main_l=m.put_m()
+        sub_l=m.put_s()
+    # for i in main_l:
+    #     print("main",i.name)
+    # for i in sub_l:
+    #     print("sub",i.name)
     return render(request, "apply/result.html",{"main": main_l, "sub":sub_l})
--- HSE/education/views.py
+++ HSE/education/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import EduPost
 from django.utils import timezone
 import datetime
-
+from apply.views import get_c
 # Create your views here.
 
 
@@ -51,6 +51,7 @@ def create(request):
     new_post.video = request.FILES.get("video")
     new_post.extrafile = request.FILES.get("extrafile")
     new_post.save()
+    get_c(new_post.id)
     return redirect("education:detail", new_post.id)
 
 
@@ -76,10 +77,12 @@ def update(request, id):
     update_post.video = request.FILES.get("video")
     update_post.extrafile = request.FILES.get("extrafile")
     update_post.save()
+    get_c(update_post.id)
     return redirect("education:detail", update_post.id)
 
 
 def delete(request, id):
     delete_post = EduPost.objects.get(id=id)
     delete_post.delete()
+    get_c(id)
     return redirect("education:edulist")
