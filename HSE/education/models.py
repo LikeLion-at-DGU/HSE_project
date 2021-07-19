bf66a76589a0f68f5ee313c06e7a41bcd5fae414
tkdwns414 <sangjune2000@naver.com>
2021-07-20 02:19:03 +0900

    explain for education

--- HSE/education/models.py
+++ HSE/education/models.py
@@ -4,16 +4,22 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class EduPost(models.Model):
-    title = models.CharField(max_length=200)
-    writer = models.ForeignKey(User, on_delete=models.CASCADE)
-    pub_date = models.DateTimeField()
-    due_date = models.DateTimeField()
-    work_date = models.DateTimeField(null=True)
-    main_teacher = models.IntegerField()
-    sub_teacher = models.IntegerField()
-    work_hour = models.IntegerField()
-    body = models.TextField()
-    video = models.CharField(null=True, max_length=200)
-    image = models.FileField(upload_to="image/", null=True)
-    extrafile = models.FileField(upload_to="extrafiles/", null=True)
-    count=models.IntegerField(default=0)
+    title = models.CharField(max_length=200)  # 봉사활동 이름
+    writer = models.ForeignKey(User, on_delete=models.CASCADE)  # 작성자, User 모델 사용
+    pub_date = models.DateTimeField()  # 글 최초 작성 일자
+    due_date = models.DateTimeField()  # 봉사활동 마감 일자
+    work_date = models.DateTimeField(null=True)  # 봉사활동 예정 일자(실제 봉사 하는 날)
+    main_teacher = models.IntegerField()  # 모집하는 주 강사 수
+    sub_teacher = models.IntegerField()  # 모집하는 보조 강사 수
+    work_hour = models.IntegerField()  # 봉사활동 인정 시간(0도 가능)
+    body = models.TextField()  # 봉사활동 관련 설명 본문
+    video = models.CharField(null=True, max_length=200)  # 유튜브에 업로드한 동영상 url
+    image = models.FileField(
+        upload_to="image/", null=True
+    )  # 봉사활동 관련 이미지(있으면 글 썸네일, 없으면 메인 로고가 글 썸네일)
+    extrafile = models.FileField(
+        upload_to="extrafiles/", null=True
+    )  # 추가적으로 필요한 파일(봉사활동 관련 pdf 등)
+    count = models.IntegerField(default=0)  # 각 봉사당 결과가 한번만 랜덤으로 선정되게 count 값을 통해 확인
+    """ count 만들기 전 봉사자로 선정된 결과를 확인할 때마다 랜덤으로 봉사자 중 뽑게돼서 
+    이를 막기 위해 count = 0일 때만 봉사자를 뽑고 그 이후로 결과 확인을 해도 뽑힌 봉사자가 변하지 않도록 설정"""
--- HSE/education/views.py
+++ HSE/education/views.py
@@ -6,21 +6,22 @@ import datetime
 
 # Create your views here.
 
-
+# 봉사활동 게시글 목록 보여주는 페이지
 def edulist(request):
     post = EduPost.objects.all()
     post = post.order_by("-pub_date")
     return render(request, "education/edulist.html", {"posts": post})
 
 
+# 봉사활동 게시글 상세 페이지
 def detail(request, id):
     post = get_object_or_404(EduPost, pk=id)
-
+    # author: 현재 유저가 게시글 작성자와 동일한지 전달하는 변수 (이후 수정하기, 삭제하기 버튼 띄울지 말지 결정)
     if request.user == post.writer:
         author = True
     else:
         author = False
-
+    # cmp에 현재 시간 받아와서 저장하고 형식 맞춘 후 pub_date와 비교하여 마감됐는지 확인하고 저장
     cmp = datetime.datetime.now()
     cmp = cmp.strftime("%y-%m-%d %H:%M:%S")
     cmp = datetime.datetime.strptime(cmp, "%y-%m-%d %H:%M:%S")
@@ -28,7 +29,7 @@ def detail(request, id):
         is_dead = True
     else:
         is_dead = False
-
+    # 봉사 게시글 정보, 글 작성자 여부, 봉사 마감 여부 전달
     return render(
         request,
         "education/detail.html",
@@ -36,21 +37,25 @@ def detail(request, id):
     )
 
 
+# 새 글 작성 페이지
 def new(request):
     return render(request, "education/new.html")
 
 
+# 새 글 작성 동작
 def create(request):
+    # 모델 설명과 동일
     new_post = EduPost()
     new_post.title = request.POST["title"]
-    new_post.writer = request.user
-    new_post.pub_date = timezone.now()
+    new_post.writer = request.user  # 현재 접속한 유저를 글 작성자로
+    new_post.pub_date = timezone.now()  # 현재 시간을 게시글 작성 시간으로
     new_post.due_date = request.POST["due_date"]
     new_post.work_date = request.POST["work_date"]
     new_post.main_teacher = request.POST["main_teacher"]
     new_post.sub_teacher = request.POST["sub_teacher"]
     new_post.work_hour = request.POST["work_hour"]
     new_post.body = request.POST["body"]
+    # 입력한 유튜브 동영상 주소에서 필요없는 부분 제하고 iframe 형식 맞추기 (& 뒤에 재생시간 정보가 있어서 제거)
     new_post.video = request.POST["video"].replace("watch?v=", "embed/")
     if "&" in new_post.video:
         new_post.video = new_post.video[: new_post.video.index("&")]
@@ -61,9 +66,10 @@ def create(request):
     return redirect("education:detail", new_post.id)
 
 
+# 기존 봉사 게시글 수정 페이지
 def edit(request, id):
     edit_post = EduPost.objects.get(id=id)
-
+    # author : 현재 접속한 유저가 수정하려는 게시글의 작성자인지 확인하고 저장
     if request.user == edit_post.writer:
         author = True
     else:
@@ -71,6 +77,7 @@ def edit(request, id):
     return render(request, "education/edit.html", {"post": edit_post, "author": author})
 
 
+# 기존 봉사 게시글 수정 동작
 def update(request, id):
     ap_all = apply.objects.all()
     update_post = EduPost.objects.get(id=id)
@@ -97,6 +104,7 @@ def update(request, id):
     return redirect("education:detail", update_post.id)
 
 
+# 봉사 게시글 삭제
 def delete(request, id):
     delete_post = EduPost.objects.get(id=id)
     delete_post.delete()
