b16372e298bd47562bf102078105701a62eb2a68
LeeTaeGeom <taegeom0601@gmail.com>
2021-07-20 01:39:38 +0900

    apply 수정, 설명

--- HSE/apply/urls.py
+++ HSE/apply/urls.py
@@ -5,9 +5,9 @@ app_name = "apply"
 urlpatterns = [
    
 
-    path('apply_main_sub/<int:post_id>',views.apply_main_sub, name="apply_main_sub"),
-    path('apply_new/',views.apply_new, name="apply_new"),
-    path('apply_result/<int:post_id>',views.apply_result, name="apply_result"),
+    path('apply_main_sub/<int:post_id>',views.apply_main_sub, name="apply_main_sub"), # post_id에 대응하는 봉사에 신청하기위해 apply_main_sub로 이동
+    path('apply_new/',views.apply_new, name="apply_new"), # db에 저장하기위해 views에 db에 저장하는 apply_new로 이동
+    path('apply_result/<int:post_id>',views.apply_result, name="apply_result"), # 결과를 보기위해 views에 apply_result로 이동
   
 
 ]
